5fd535e84e4a1483786ff3172a22053d9369b6a4
PRIYANSHU <priyanshuask528@gmail.com>
2026-06-24 16:02:09 +0530

    Create account portal formed

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
-
+import json
+import random
+import string
+from pathlib import Path
 
 
 class Bank:
+    database="data.json"
+    data = []
+    try:
+        if Path(database).exists():
+            with open(database,'r') as fs:
+                data=json.loads(fs.read()) 
+        else :
+            print("No such file exists")
+    except Exception as err :
+        print(f"an exception occured as {err}")
+    @staticmethod
+    
+    def update() :
+        with open(Bank.database,'w') as fs :
+            fs.write(json.dumps(Bank.data))
+
     def createaccount(self):
-        pass
+        info={
+            "name":input("Tell your name:-") ,
+            "age" : int(input("Tell your age:-")),
+            "email" : input("Enter your email:-"),
+            "pin" : int(input("Tell your 4 number pin:-"))
+            "accountNo." : 1234 , 
+            "balance" : 0 
+        }
+
+
+    if info['age']<18 or len(str(info['pin'])) !=4 :
+        print("Sorry you cannot create your account.")
+    else :
+        print("Account has been created successfully")
+        for i in data :
+            print(f"{i}:{info[i]}")
+        print("Please note down your account number.")
+        Bank.data.append(info)
+        
+
 
 user=Bank() :
 
