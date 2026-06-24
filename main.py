6a6fec40069e6d78ca67d1456f8a66db5fe754c8
PRIYANSHU <priyanshuask528@gmail.com>
2026-06-24 12:45:00 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Press 1 for creating an account.")
+print("Press 2 for deposite money in the bank.")
+print("Press 3 for withdrawing the money.")
+print("Press 4 for details.")
+print("Press 5 for updating the details.")
+print("Press 6 for deleting your account.")
+
